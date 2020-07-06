2d28278de6b7061fa42a5fa615e03978b34ed253
cavenaghi <cavenaghi1988@yahoo.gr>
2020-07-07 00:59:11 +0300

    adapt

--- src/main.js
+++ src/main.js
@@ -1,46 +1,40 @@
-let iframe = document.querySelectorAll("iframe")
-
+let iframe = document.querySelectorAll("iframe");
 
 let liframe = document.getElementById("left");
 let riframe = document.getElementById("right");
 let tiframe = document.getElementById("top");
 let main = document.getElementById("main-site");
 
-let oldIframe = iframe.contentDocument;
 
 window.onload = function () {
-  if (iframe) {
-    setInterval(() => {
-      let newiframe = iframe.contentDocument;
-      if (newiframe == oldIframe) {
-        return
-      } else {
-        oldIframe = newiframe
-      }
-    }, 500);
-  }
-
-}
-
-
+  setInterval(() => {
+    if (window.matchMedia("(max-width: 770px)").matches) {
+      tiframe.contentWindow.postMessage(`600px`, "*");
+    } else if (window.matchMedia("(max-width: 800px)").matches) {
+      tiframe.contentWindow.postMessage(`800px`, "*");
+    } else if (window.matchMedia("(max-width: 920px)").matches) {
+      tiframe.contentWindow.postMessage(`920px`, "*");
+    } else if (window.matchMedia("(max-width: 1121px)").matches) {
+      tiframe.contentWindow.postMessage(`1121px`, "*");
+    }
+  }, 500);
+};
 
 window.onscroll = function () {
   let rect = main.getBoundingClientRect();
   let rectTop = tiframe.getBoundingClientRect();
 
   if (rectTop) {
-    tiframe.contentWindow.postMessage(`${rectTop.top}`, '*')
-
+    tiframe.contentWindow.postMessage(`${rectTop.top}`, "*");
   }
-
 
   if (rect.top < 0) {
-    liframe.contentWindow.postMessage("active", '*')
-    riframe.contentWindow.postMessage("active", "*")
-    return false
+    liframe.contentWindow.postMessage("active", "*");
+    riframe.contentWindow.postMessage("active", "*");
+    return false;
   } else {
-    liframe.contentWindow.postMessage("", '*')
-    riframe.contentWindow.postMessage("", "*")
-    return false
+    liframe.contentWindow.postMessage("", "*");
+    riframe.contentWindow.postMessage("", "*");
+    return false;
   }
-}
+};
--- src/top/top.js
+++ src/top/top.js
@@ -1,4 +1,6 @@
 let bg = document.querySelector(".bg")
+let video = document.querySelector(".video")
+let text = document.querySelector(".text")
 
 
 document.body.addEventListener("click", function () {
@@ -11,5 +13,28 @@ window.onload = function () {
     if (event.data) {
       bg.style.transform = (`translate(-50%, ${event.data * -1}px)`)
     }
+
+
+    if (event.data == "600px") {
+      bg.classList.add("active")
+      video.style.display = "none";
+      text.style.left = "157px";
+      text.style.width = "50%";
+    } else if (event.data == "800px") {
+      bg.classList.remove("active")
+      video.style.display = "none";
+      text.style.left = "181px";
+      text.style.width = "54%";
+    } else if (event.data == "920px") {
+      video.style.marginRight = "2px";
+      video.style.display = "inline-block";
+      video.style.height = "130px";
+      text.style.left = "-105px";
+      text.style.width = "28%";
+    } else if (event.data == "1121px") {
+      video.style.display = "inline-block";
+      video.style.height = "203px";
+    }
+
   })
 }
